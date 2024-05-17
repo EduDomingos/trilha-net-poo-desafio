beedc18d5c3ec9d3d0e5ff164d68268396b10d88
edudomingos <edudomingos@gmail.com>
2024-05-17 19:41:59 -0300

    Resolvido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+var myIphone = new Iphone("14 99999-9999", "15", "654321567984231", 256);
+var myNokia = new Nokia("16 99999-9999", "LS-22", "6542313635874168", 128);
+
+myIphone.Ligar();
+myNokia.ReceberLigacao();
+
+myIphone.InstalarAplicativo("Facetime");
+myNokia.InstalarAplicativo("WhatsApp");
